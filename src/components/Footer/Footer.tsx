69960a2b38c20dd40e0e31a92b7772bdfa477640
Askhad Arzybaev <aarzybaev@gmail.com>
2024-02-06 12:02:19 +0600

    created Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light mt-4">
+      <div className="container py-3">
+        <div className="row">
+          <div className="col-md-6">
+            <p>&copy; 2024 Your Website. All rights reserved.</p>
+          </div>
+          <div className="col-md-6 text-end">
+            <a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
