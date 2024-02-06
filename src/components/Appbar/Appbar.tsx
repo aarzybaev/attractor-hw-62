8278ad60d73935ae5203f764731d0c37b925d8ef
Askhad Arzybaev <aarzybaev@gmail.com>
2024-02-06 12:02:58 +0600

    created Appbar component

---
+++ src/components/Appbar/Appbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const Appbar: React.FC = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container-fluid">
+        <span className="navbar-brand">HW-62</span>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">About</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/contacts">Contacts</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Appbar;
